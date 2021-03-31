373d0707b2ef861af128127bd16f6637606b6383
richard hopkins <richard.t.hopkins@outlook.com>
2021-04-01 01:24:27 +1100

    added in moment.js for the times and working on the past present future colour coding

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -5,5 +5,36 @@ var now = moment().format('dddd, MMMM Do');
 dateElement.textContent = now;
 
 
+//get the current time and apply changes to the text area style.
+var timeLable = ['8', '9' , '10' , '11' , '12', '13' , '14' , '15' , '16' , '17'] 
+var currentHour = moment().format('H');
+
+for(var i =  0; i <= timeLable.length; i++){
+
+    if(timeLable[i] === currentHour){
+
+        $('label-'+ i).attr('class') = "present";
+
+    }else if(timeLable[i] < currentHour){
+
+        $('label-'+i).attr('class') = "past";
+
+    }else{
+
+        $('label-'+i).attr('class') = "future";
+
+    }
+
+}
+
+//get the contents of the text areas.
+
+
+
+// save the information that is in the text area when the save button is clicked.
+
+
+
+
 
 
