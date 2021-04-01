ce0f45fb1c48259cc01f22546e86823f588c7574
Richard Hopkins <richard.t.hopkins@outlook.com>
2021-04-01 23:54:20 +1100

    made finale changes to javascript

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -5,63 +5,70 @@ var now = moment().format('dddd, MMMM Do');
 dateElement.textContent = now;
 
 //variable for the save buttons
-var saveButton8 = $('#save-8');
-var saveButton9 = $('#save-9');
-var saveButton10 = $('#save-10');
-var saveButton11 = $('#save-11');
-var saveButton12 = $('#save-12');
-var saveButton13 = $('#save-13');
-var saveButton14 = $('#save-14');
-var saveButton15 = $('#save-15');
-var saveButton16 = $('#save-16');
-var saveButton17 = $('#save-17');
 
+var saveButton = $('.saveBtn');
+
+
+//gets the text areas by id.
+var textArea8 = $('#text-8');
+var textArea9 = $('#text-9');
+var textArea10 = $('#text-10');
+var textArea11 = $('#text-11');
+var textArea12 = $('#text-12');
+var textArea13 = $('#text-13');
+var textArea14 = $('#text-14');
+var textArea15 = $('#text-15');
+var textArea16 = $('#text-16');
+var textArea17 = $('#text-17');
+
+
+//save text to textBlocks object.
 function saveTextAreas(){
 
-    var textArea8 = $('#text-8');
-    var textArea9 = $('#text-9');
-    var textArea10 = $('#text-10');
-    var textArea11 = $('#text-11');
-    var textArea12 = $('#text-12');
-    var textArea13 = $('#text-13');
-    var textArea14 = $('#text-14');
-    var textArea15 = $('#text-15');
-    var textArea16 = $('#text-16');
-    var textArea17 = $('#text-17');
-
-    console.log(textArea8.val);
-
     var textBlocks = {
-        text8: textArea8.value,
-        text9: textArea9.value,
-        text10: textArea10.value,
-        text11: textArea11.value,
-        text12: textArea12.value,
-        text13: textArea13.value,
-        text14: textArea14.value,
-        text15: textArea15.value,
-        text16: textArea16.value,
-        text17: textArea17.value
+        text8: textArea8.val(),
+        text9: textArea9.val(),
+        text10: textArea10.val(),
+        text11: textArea11.val(),
+        text12: textArea12.val(),
+        text13: textArea13.val(),
+        text14: textArea14.val(),
+        text15: textArea15.val(),
+        text16: textArea16.val(),
+        text17: textArea17.val()
     };
     localStorage.setItem("textBlocks", JSON.stringify(textBlocks));
 }
 
 
-
-saveButton8.on("click", function (event){
+//save text areas to local storage.
+saveButton.on("click", function (event){
     event.preventDefault();
     saveTextAreas();
-    
 });
 
-// function showEvents() {
-//     var hourlyEvents = JSON.parse(localStorage.getItem("textBlocks"));
-//     if (hourlyEvents !== null) {
-//     //   document.querySelector(".message").textContent = lastGrade.student + 
-//     //   " received a/an " + lastGrade.grade
-//     }
 
-// }
+//function to show events from local storage.
+function showEvents() {
+     var hourlyEvents = JSON.parse(localStorage.getItem("textBlocks"));
+     if (hourlyEvents !== null) {
+        document.getElementById("text-8").innerHTML = hourlyEvents.text8;
+        document.getElementById("text-9").innerHTML = hourlyEvents.text9;
+        document.getElementById("text-10").innerHTML = hourlyEvents.text10;
+        document.getElementById("text-11").innerHTML = hourlyEvents.text11;
+        document.getElementById("text-12").innerHTML = hourlyEvents.text12;
+        document.getElementById("text-13").innerHTML = hourlyEvents.text13;
+        document.getElementById("text-14").innerHTML = hourlyEvents.text14;
+        document.getElementById("text-15").innerHTML = hourlyEvents.text15;
+        document.getElementById("text-16").innerHTML = hourlyEvents.text16;
+        document.getElementById("text-17").innerHTML = hourlyEvents.text17;
+     }
+};
+
+//initiates the page on load.
+function init(){
+    showEvents();
+};
 
 //get the current time and apply changes to the text area style.
 var timeLabel = ['8', '9' , '10' , '11' , '12', '13' , '14' , '15' , '16' , '17'] 
@@ -88,6 +95,7 @@ for(var i =  0; i <= timeLabel.length -1; i++){
 }
 
 
+init();
 
 
 
